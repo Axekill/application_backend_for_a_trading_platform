bdea42537ae06feb6921fb5f3ffc1e2677de4992
Andrey <andrei3.ru@ya.ru>
2023-11-08 20:15:40 +0300

    добавил модели , контролелры , сервисы , мапер, пока без логики

--- src/main/java/ru/skypro/homework/dto/UserDTO.java
+++ src/main/java/ru/skypro/homework/dto/UserDTO.java
@@ -1,3 +1,28 @@
 package ru.skypro.homework.dto;
+
+import lombok.*;
+import ru.skypro.homework.model.Role;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class UserDTO {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+    private String image;
 }
--- src/main/java/ru/skypro/homework/model/User.java
+++ src/main/java/ru/skypro/homework/model/User.java
@@ -1,18 +1,24 @@
 package ru.skypro.homework.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
     private String email;
     private String firstName;
     private String lastName;
