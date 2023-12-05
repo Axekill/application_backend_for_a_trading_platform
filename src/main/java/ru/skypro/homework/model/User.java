9a1381adb86b3e12bff2d69bbcd7824c36d46399
Александр <egorrak3221@mail.ru>
2023-12-05 17:14:21 +0300

    security

--- src/main/java/ru/skypro/homework/model/User.java
+++ src/main/java/ru/skypro/homework/model/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Table(name = "User")
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,6 +33,8 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+
 
     @OneToMany(mappedBy = "author")
     private List<Ad> adList;
